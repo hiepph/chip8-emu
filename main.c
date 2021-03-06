f2f9e2d2b36b49b96d98193a1824d925e66a1e5f
Hiep Pham <hiepph.2406@gmail.com>
2021-03-06 23:02:16 +0700

    chore: remove definitions in main

--- main.c
+++ main.c
@@ -112,17 +112,5 @@ int main(int argc, char **argv) {
     myChip8.setKeys();
   }
 
-  unsigned short opcode;
-  unsigned char memory[4096];
-  unsigned char V[16];
-  unsigned short I;
-  unsigned short pc;
-  unsigned char gfx[64 * 32];
-  unsigned char delay_timer;
-  unsigned char sound_timer;
-  unsigned short stack[16];
-  unsigned short sp;
-  unsigned char key[16];
-
-  return 0
+  return 0;
 }
