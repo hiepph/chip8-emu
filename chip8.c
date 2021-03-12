4b55b7032d7f6b2a47423c2e2573321cbd4c3c50
Hiep <hiepph.2406@gmail.com>
2021-03-12 16:19:56 +0700

    feat: implementation of 0xB., 0xC., 0xD., a part of 0xF.

--- chip8.c
+++ chip8.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #include "display.h"
@@ -114,8 +114,8 @@ void TableE(Chip8* ch8, uint16_t opcode) {
   tableE[opcode & 0x000F](ch8, opcode);
 }
 
-void TableE(Chip8* ch8, uint16_t opcode) {
-  tableE[opcode & 0x000F](ch8, opcode);
+void TableF(Chip8* ch8, uint16_t opcode) {
+  tableF[opcode & 0x000F](ch8, opcode);
 }
 
 // clear the screen
@@ -312,6 +312,88 @@ void OP_Annn(Chip8* ch8, uint16_t opcode) {
   ch8->I = nnn;
 }
 
+// PC = V0 + nnn
+void OP_Bnnn(Chip8* ch8, uint16_t opcode) {
+  uint16_t nnn = opcode & 0x0FFF;
+  ch8->pc = ch8->V[0] + nnn;
+}
+
+// Vx = rand() & nn
+void OP_Cxnn(Chip8* ch8, uint16_t opcode) {
+  uint8_t x = (opcode & 0x0F00) >> 8;
+  uint8_t nn = opcode & 0x00FF;
+  ch8->V[x] = rand() & nn;
+}
+
+// draw(Vx, Vy, n)
+// Vf = collision
+void OP_Dxyn(Chip8* ch8, uint16_t opcode) {
+  uint8_t x = (opcode & 0x0F00) >> 8;
+  uint8_t y = (opcode & 0x00F0) >> 4;
+  uint8_t n = opcode & 0x000F;
+
+  // wrap if going beyond screen boundaries
+  uint8_t x_pos = ch8->V[x] % DISPLAY_WIDTH;
+  uint8_t y_pos = ch8->V[y] % DISPLAY_HEIGHT;
+
+  ch8->V[0xF] = 0;
+  // draw 8x(N+1) sprites
+  for (unsigned int row = 0; row <= n; ++row) {
+    uint8_t sprite_byte = ch8->memory[ch8->I + row];
+    for (unsigned int col = 0; col < 8; col++) {
+      uint8_t sprite_pixel = sprite_byte & (0x80 >> col);
+      uint32_t* screen_pixel = &ch8->display[(y_pos + row) * DISPLAY_WIDTH + (x_pos + col)];
+
+      if (sprite_pixel) {
+        // collision
+        if (*screen_pixel == 0xFFFFFFFF) {
+          ch8->V[0xF] = 1;
+        }
+
+        *screen_pixel ^= 0xFFFFFFFF;
+      }
+    }
+  }
+}
+
+// Vx = get_delay()
+void OP_Fx07(Chip8* ch8, uint16_t opcode) {
+  uint8_t x = (opcode & 0x0F00) >> 8;
+  ch8->V[x] = ch8->delay_timer;
+}
+
+// Vx = get_key()
+void OP_Fx0A(Chip8* ch8, uint16_t opcode) {
+
+}
+
+// delay_timer(Vx)
+void OP_Fx15(Chip8* ch8, uint16_t opcode) {
+  uint8_t x = (opcode & 0x0F00) >> 8;
+  ch8->delay_timer = ch8->V[x];
+}
+
+// sound_timer(V[x])
+void OP_Fx18(Chip8* ch8, uint16_t opcode) {
+  uint8_t x = (opcode & 0x0F00) >> 8;
+  ch8->sound_timer = ch8->V[x];
+}
+
+// I += V[x]
+void OP_Fx1E(Chip8* ch8, uint16_t opcode) {
+  uint8_t x = (opcode & 0x0F00) >> 8;
+  ch8->I += ch8->V[x];
+}
+
+// I = sprite_addr[Vx]
+void OP_Fx29(Chip8* ch8, uint16_t opcode) {
+}
+
+void OP_Fx33(Chip8* ch8, uint16_t opcode) {}
+void OP_Fx55(Chip8* ch8, uint16_t opcode) {}
+void OP_Fx65(Chip8* ch8, uint16_t opcode) {}
+
+
 // initialize the Chip8
 void initialize(Chip8* ch8) {
   ch8->pc = START_ADDRESS;
@@ -344,7 +426,7 @@ void initialize(Chip8* ch8) {
   table[0xC] = OP_Cxnn;
   table[0xD] = OP_Dxyn;
   table[0xE] = TableE;
-  /* table[0xF] = TableF; */
+  table[0xF] = TableF;
 
   table0[0x0] = OP_00E0;
   table0[0xE] = OP_00EE;
@@ -362,15 +444,15 @@ void initialize(Chip8* ch8) {
   tableE[0x1] = OP_ExA1;
   tableE[0xE] = OP_Ex9E;
 
-  /* tableF[0x07] = OP_Fx07; */
-  /* tableF[0x07] = OP_Fx07; */
-  /* tableF[0x07] = OP_Fx07; */
-  /* tableF[0x07] = OP_Fx07; */
-  /* tableF[0x07] = OP_Fx07; */
-  /* tableF[0x07] = OP_Fx07; */
-  /* tableF[0x07] = OP_Fx07; */
-  /* tableF[0x15] = OP_Fx15; */
-  /* tableF[0x65] = OP_Fx65; */
+  tableF[0x07] = OP_Fx07;
+  tableF[0x0A] = OP_Fx0A;
+  tableF[0x15] = OP_Fx15;
+  tableF[0x18] = OP_Fx18;
+  tableF[0x1E] = OP_Fx1E;
+  tableF[0x29] = OP_Fx29;
+  tableF[0x33] = OP_Fx33;
+  tableF[0x55] = OP_Fx55;
+  tableF[0x65] = OP_Fx65;
 }
 
 // load application into chip 8
@@ -406,6 +488,9 @@ void cycle(Chip8* ch8) {
 
 
 int main(int argc, char *argv[]) {
+  // initialize random seed
+  srand(time(NULL));
+
   if (argc != 4) {
     printf("USAGE: %s [scale] [delay] [ROM]\n", argv[0]);
     exit(1);
